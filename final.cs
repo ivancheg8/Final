1beec1b2baf26ddc5aef08e07f65b5806dfb98e2
ivancheg8 <pavlevan@yandex.ru>
2022-08-08 22:15:31 +0300

    add code programm

--- final.cs
+++ final.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] names = { "hello", "2", "world", ":-)" };
+
+string[] ArrayLessThree(string[] words)
+{
+    int count = 0;
+    string[] temp = new string[words.Length];
+    for (int i = 0; i < words.Length; i++)
+        if (words[i].Length <= 3)
+        {
+            temp[count] = words[i];
+            count++;
+        }
+    Array.Resize(ref temp, count);
+    return temp;
+}
+
+void ShowArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++)
+        Console.Write(str[i] + ", ");
+}
+
+ShowArray(ArrayLessThree(names));
